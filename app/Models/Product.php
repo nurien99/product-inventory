68b6710a0cd44c80942096fff9f9eec5dc0dca52
NURIN ATHIRAH MOHD NASIR <nurienathirah@gmail.com>
2025-12-28 21:19:20 +0800

    Make migration and define product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock'
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'stock' => 'integer'
+    ];
 }
